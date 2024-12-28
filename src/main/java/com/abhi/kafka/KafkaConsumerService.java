a2ce96bac120b9de440563826f687b4e2746cab3
Abhinav <abhisetu610@gmail.com>
2024-12-28 20:23:21 +0530

    consumer group understanding

--- src/main/java/com/abhi/kafka/KafkaConsumerService.java
+++ src/main/java/com/abhi/kafka/KafkaConsumerService.java
@@ -10,4 +10,15 @@ public class KafkaConsumerService {
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
     }
+
+    //will get same message what consume/consumeInSameGroup gets
+    @KafkaListener(topics = "my-topic", groupId = "my-group-2")
+    public void consumeGroup2(String message) {
+        System.out.println("Consumed message from group - 2: " + message);
+    }
+    // either this or my-topic will get message since there is only 1 partition
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void consumeInSameGroup(String message) {
+        System.out.println("Consumed message by second consumer from group 1: " + message);
+    }
 }
